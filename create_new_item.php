fa51e9b29135b0fc5e50550067d6346b89f35236
csg33k <balajiyaswanth.v@gmail.com>
2021-11-19 23:43:15 +0530

    done for the day

--- create_new_item.php
+++ create_new_item.php
@@ -1,3 +1,16 @@
+<?php
+    $pdo = new PDO("mysql:host=localhost;port=3306;dbname=products_crud","root","");
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $statement = $pdo->prepare('SELECT * FROM products');
+    $statement->execute();
+    $products = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+    echo '<pre>';
+    var_dump($_POST);
+    echo '</pre>';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
